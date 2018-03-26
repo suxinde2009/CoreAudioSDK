907086344f045f315827df9430b96404f06bf056
suxinde2009 <suxinde2009@126.com>
2018-03-26 17:59:32 +0800

    Init，将对其进行裁剪，以适配iOS开发

---
+++ SPCoreAudioSDK/SPCoreAudioSDK.h
@@ -0,0 +1,10 @@
+//
+//  SPCoreAudioSDK.h
+//  SPCoreAudioSDK
+//
+//  Created by SuXinDe on 2018/3/26.
+//  Copyright © 2018年 SkyPrayer Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
---
+++ SPCoreAudioSDK/SPCoreAudioSDK.mm
@@ -0,0 +1,11 @@
+//
+//  SPCoreAudioSDK.m
+//  SPCoreAudioSDK
+//
+//  Created by SuXinDe on 2018/3/26.
+//  Copyright © 2018年 SkyPrayer Studio. All rights reserved.
+//
+
+#import "SPCoreAudioSDK.h"
+
+
